9f99534320061daee2ebec8434d52925e6aa6594
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-05-10 23:58:55 +0300

    Adding Page model

--- contact/models.py
+++ contact/models.py
@@ -2,11 +2,32 @@ from django.db import models
 
 # Create your models here.
 
-# Creating a Agent table
+# Creating a Skill table
 class Skill(models.Model):
-    #Map 1 User to 1 Agent
     skill_name = models.CharField(max_length=30)
     skill_percent = models.IntegerField(default=20)
 
     def __str__(self):
         return self.skill_name
+
+# Creating a WebPage table
+class Page(models.Model):
+    page_link = models.CharField(max_length=100)
+    page_fontawesomeclass = models.CharField(max_length=50)
+    page_color = models.CharField(max_length=10)
+    page_name = models.CharField(max_length=20)
+
+    socialmedia = 'socialmedia'
+    business = 'business'
+    chioces = [
+        (socialmedia, 'socialmedia'),
+        (business, 'business'),
+    ]
+    page_category = models.CharField(
+        max_length=20,
+        choices=chioces,
+        default=socialmedia,
+    )
+
+    def __str__(self):
+        return self.page_name
