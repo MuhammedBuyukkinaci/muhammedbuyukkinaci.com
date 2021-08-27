267ca510aa365fcee3e36cfc40e9db0109f30ff2
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-08-27 22:20:20 +0300

    Making home app dynamic

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Welcome Page Text
+class Homepage(models.Model):
+
+    home_summary = models.CharField(max_length=50)
+    home_text = models.TextField()
+
+    def __str__(self):
+        return self.home_summary
--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView,View
-from contact.models import Skill
+from django.views.generic import ListView
+from .models import Homepage
+from random import randint
 
 # Create your views here.
 
 class HomeView(ListView):
-    model = Skill
+    model = Homepage
     template_name = "home/home.html"
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        count = Homepage.objects.count()
+        data['homepage'] = Homepage.objects.all()[randint(0, count - 1)] 
+        return data
