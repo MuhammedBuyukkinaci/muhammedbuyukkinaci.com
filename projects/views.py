b349bd223bc2603c63cc825a68dde5a1401c01d5
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-08-27 22:23:07 +0300

    Making randomly selection operation more efficient

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
 from django.views.generic import ListView 
-
+import random
 from .models import Project,Detail
+from random import shuffle
 
 class ProjectView(ListView):
     template_name = "projects/projects.html"
-    queryset = Project.objects.all().order_by('?')
-    context_object_name = "projects"
+    model = Project
+
 
     def get_context_data(self, **kwargs):
         data = super().get_context_data(**kwargs)
+        data['projects'] = sorted(Project.objects.all(), key=lambda x: random.random())
         data['details'] = Detail.objects.all().order_by('detail_number')
         return data
