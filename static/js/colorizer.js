c14942c38f18d4b52d642a547cd766cb48119d9c
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-08-27 22:22:25 +0300

    Commenting to be used later

--- static/js/colorizer.js
+++ static/js/colorizer.js
@@ -1,39 +1,39 @@
-var page_title = document.getElementsByTagName("title")[0].innerHTML.split(' - ')[0];
-var sidebar_list = document.getElementById("sidebar_ul").getElementsByTagName("a");
-var navbar_list = document.getElementById("navbar_ul").getElementsByTagName("a");
-var my_home_button = document.getElementById('my_home_button');
+// var page_title = document.getElementsByTagName("title")[0].innerHTML.split(' - ')[0];
+// var sidebar_list = document.getElementById("sidebar_ul").getElementsByTagName("a");
+// var navbar_list = document.getElementById("navbar_ul").getElementsByTagName("a");
+// var my_home_button = document.getElementById('my_home_button');
 
-for (var i = 0; i < sidebar_list.length; i++) {
-    if (sidebar_list[i].innerHTML === page_title) {
-        sidebar_list[i].classList.add("active");
-        navbar_list[i].classList.add("active");
-        navbar_list[i].classList.add("text-white");
-    } else {
-        sidebar_list[i].parentElement.classList.add('non_visible');
-    }
-}
+// for (var i = 0; i < sidebar_list.length; i++) {
+//     if (sidebar_list[i].innerHTML === page_title) {
+//         sidebar_list[i].classList.add("active");
+//         navbar_list[i].classList.add("active");
+//         navbar_list[i].classList.add("text-white");
+//     } else {
+//         sidebar_list[i].parentElement.classList.add('non_visible');
+//     }
+// }
 
-switch (page_title) {
-    case 'Home':
-        class_to_add = "btn-secondary";
-        break;
-    case 'Contact':
-        class_to_add = "btn-success";
-        break;
-    case 'You Know Me?':
-        class_to_add = "btn-danger";
-        break;
-    case 'Testimonials':
-        class_to_add = "btn-warning";
-        break;
-    case 'Photos':
-        class_to_add = "btn-info";
-        break;
-    case 'Projects':
-        class_to_add = "btn-primary";
-        break;
-    case 'Monetary Metrics':
-        class_to_add = "btn-dark";
-}
+// switch (page_title) {
+//     case 'Home':
+//         class_to_add = "btn-secondary";
+//         break;
+//     case 'Contact':
+//         class_to_add = "btn-success";
+//         break;
+//     case 'You Know Me?':
+//         class_to_add = "btn-danger";
+//         break;
+//     case 'Testimonials':
+//         class_to_add = "btn-warning";
+//         break;
+//     case 'Photos':
+//         class_to_add = "btn-info";
+//         break;
+//     case 'Projects':
+//         class_to_add = "btn-primary";
+//         break;
+//     case 'Monetary Metrics':
+//         class_to_add = "btn-dark";
+// }
 
-my_home_button.classList.add(class_to_add);
+// my_home_button.classList.add(class_to_add);
