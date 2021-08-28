df38e3d23f36319d4c9d949ea085cb3780d34d89
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-08-28 11:52:45 +0300

    Changing admin page for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,13 @@ from .models import Category, Article
 # Register your models here.
 
 class ArticleAdmin(admin.ModelAdmin):
+    
+    #Which fields to show on admin page before changing
+    list_display = ("article_title", "article_category",)
+    # A search bar to search input on the below area
+    search_fields = ("article_explanation",)
+    # Filter options shown in the right section
+    list_filter = ("article_category", )
 
     fieldsets = [("Main",{"fields":["article_title","article_explanation","article_category","article_slug","article_owner"]}),
     ("Subsidiary",{"fields":["article_time","article_source"]})
