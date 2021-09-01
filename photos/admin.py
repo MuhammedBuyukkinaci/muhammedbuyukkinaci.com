95b126a6245f21102d1262e3bbee5a5b80ad9491
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-09-01 08:44:05 +0300

    Modifying admin page of Photos app

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Image
+
+class ImageAdmin(admin.ModelAdmin):
+    
+    #Which fields to show on admin page before changing
+    list_display = ("time", "location",)
+
+admin.site.register(Image,ImageAdmin)
