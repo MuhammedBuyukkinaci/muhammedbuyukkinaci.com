ba3be9d170879acc9761494d111106f7e5049c78
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-09-11 09:00:20 +0300

    Adding Preview attribute to Photos Admin

--- photos/admin.py
+++ photos/admin.py
@@ -7,5 +7,12 @@ class ImageAdmin(admin.ModelAdmin):
     
     #Which fields to show on admin page before changing
     list_display = ("time", "location",)
+    readonly_fields = ('thumbnail_preview',)
+
+    def thumbnail_preview(self, obj):
+        return obj.thumbnail_preview
+
+    thumbnail_preview.short_description = 'Image Preview'
+    thumbnail_preview.allow_tags = True
 
 admin.site.register(Image,ImageAdmin)
--- photos/models.py
+++ photos/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from PIL import Image
+from django.utils.html import mark_safe
 
 # Create your models here.
 
@@ -8,6 +9,11 @@ class Image(models.Model):
     location = models.CharField(max_length=40)
     image = models.ImageField(upload_to='images/')
 
+    @property
+    def thumbnail_preview(self):
+        if self.image:
+            return mark_safe('<img src="{}" width="300" height="300" />'.format(self.image.url))
+        return ""
 
     def __str__(self):
         return self.location
