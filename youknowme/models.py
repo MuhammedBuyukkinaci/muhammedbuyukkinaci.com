f2ac681749368d3ccfe8c5763e02b1b91cd083c7
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-10-03 16:22:53 +0300

    Adding Quiz model to youknowme app; Brining js code from static to partial view; Adding admin page for youknowme app

--- youknowme/models.py
+++ youknowme/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+# Welcome Page Text
+class Quiz(models.Model):
+
+    quiz_question = models.CharField(max_length=200)
+    quiz_answer1 = models.CharField(max_length=50)
+    quiz_answer2 = models.CharField(max_length=50)
+    quiz_answer3 = models.CharField(max_length=50)
+    quiz_answer4 = models.CharField(max_length=50)
+    quiz_correct_answer = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.quiz_question
