9d65c6b50215903f9a44a8fe031ca5ba76f6cad3
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2021-10-03 14:21:33 +0300

    Making youknowme app kind of dynamic

--- youknowme/views.py
+++ youknowme/views.py
@@ -7,3 +7,9 @@ from contact.models import Skill, Page
 class YouKnowMeView(ListView):
     model = Skill
     template_name = "youknowme/youknowme.html"
+
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data['choices'] = range(4)
+        return data
