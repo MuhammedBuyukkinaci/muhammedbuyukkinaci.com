c88830e61e6264e23152e557054df6581fd32a5c
MuhammedBuyukkinaci <mubuyuk51@gmail.com>
2022-01-14 22:59:47 +0300

    Python django debug setting fix

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -29,7 +29,7 @@ import json
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', True)
+DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '127.0.0.1').split(',')
 
